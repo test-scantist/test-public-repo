e7378201b3580433ad9fff44853bf380d25e756e
utsavgarg <utsav1996@ymail.com>
2017-10-11 19:33:46 +0800

    updated training logs

--- Assignment_1/cal_regression.py
+++ Assignment_1/cal_regression.py
@@ -122,7 +122,7 @@ def train_test_cv(filename, test_filename, num_hidden_units, learning_rate, log_
             train_writer = tf.summary.FileWriter("./tb_logs/"+log_file)
             train_writer.add_graph(tf.get_default_graph())
             sess.run(tf.global_variables_initializer())
-            logs = {'epoch': [], 'train_loss': [], 'train_acc': [], 'val_acc': [], 'test_acc': 0}
+            logs = {'epoch': [], 'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': [], 'test_acc': 0}
             for epoch in range(NUM_EPOCHS):
                 x_train, y_train = shuffle_data(x_train, y_train)
                 train_loss, train_acc, train_counter = 0, 0, 0
@@ -133,16 +133,18 @@ def train_test_cv(filename, test_filename, num_hidden_units, learning_rate, log_
                     train_loss += step['Loss']
                     train_acc += step['Accuracy']
                     train_counter += 1
-                val_acc, val_counter = 0, 0
+                val_loss, val_acc, val_counter = 0, 0, 0
                 for i in range(val_iters):
-                    step = sess.run(train_test_step['Accuracy'], feed_dict={
+                    va_lo, va_ac  = sess.run([train_test_step['Loss'], train_test_step['Accuracy']], feed_dict={
                         x: x_val[i*BATCH_SIZE:i*BATCH_SIZE+BATCH_SIZE],
                         y: y_val[i*BATCH_SIZE:i*BATCH_SIZE+BATCH_SIZE]})
-                    val_acc += step
+                    val_acc += va_ac
+		    val_loss += va_lo
                     val_counter += 1
                 logs['epoch'].append(epoch)
                 logs['train_loss'].append(train_loss/train_counter)
                 logs['train_acc'].append(train_acc/train_counter)
+                logs['val_loss'].append(val_loss/val_counter)
                 logs['val_acc'].append(val_acc/val_counter)
             test_acc, test_counter = 0, 0
             for i in range(test_iters):
@@ -207,8 +209,8 @@ def train_test(filename, test_filename, num_hidden_units, lr_1, lr_2, log_file, 
 def parameter_search(filename, test_filename):
     learning_rate = [1e-3, 0.5*1e-3, 1e-4, 0.5*1e-4, 1e-5]
     num_hidden_units_params = [20, 40, 50, 60]
-    lr = 1e-5
-    for num_units in num_hidden_units_params:
+    num_units = 30
+    for lr in learning_rate:
         log_file = '3_layer_mlp_%d_%s' % (num_units, lr)
         train_test_cv(filename, test_filename, num_units, lr, log_file)
 
@@ -227,8 +229,8 @@ def main():
     parser.add_argument('--mlp_layers', default=3, help='Number of layers in the mlp')
 
     args = parser.parse_args()
-    #parameter_search(args.train_data, args.test_data)
-    run_mlp(args.train_data, args.test_data)
+    parameter_search(args.train_data, args.test_data)
+    #run_mlp(args.train_data, args.test_data)
 
 
 if __name__ == "__main__":
