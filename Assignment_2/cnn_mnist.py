986247b265e5d6a21a476dcca59e0860c3b4a1a0
shantanukamath <shantanukamath@live.in>
2017-11-12 00:57:36 +0800

    Fixed learning rate decay configurations

--- Assignment_2/cnn_mnist.py
+++ Assignment_2/cnn_mnist.py
@@ -35,46 +35,46 @@ def deepnn(x, save_image=False):
 
     # First convolutional layer - maps one grayscale image to 15 feature maps.
     # Output shape = [-1, 20, 20, 15]
-    with tf.name_scope('conv1'):
-        W_conv1 = weight_variable([9, 9, 1, 15])
-        b_conv1 = bias_variable([15])
-        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    with tf.name_scope('C1'):
+        W_C1 = weight_variable([9, 9, 1, 15])
+        b_C1 = bias_variable([15])
+        C1 = tf.nn.relu(conv2d(x_image, W_C1) + b_C1)
 
     # Pooling layer - downsamples by 2X.
     # Output shape = [-1, 10, 10, 15]
     with tf.name_scope('pool1'):
-        h_pool1 = max_pool_2x2(h_conv1)
+        S1 = max_pool_2x2(C1)
 
     # Second convolutional layer -- maps 15 feature maps to 20.
     # Output shape = [-1, 6, 6, 20]
     with tf.name_scope('conv2'):
-        W_conv2 = weight_variable([5, 5, 15, 20])
-        b_conv2 = bias_variable([20])
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+        W_C2 = weight_variable([5, 5, 15, 20])
+        b_C2 = bias_variable([20])
+        C2 = tf.nn.relu(conv2d(S1, W_C2) + b_C2)
 
     # Second pooling layer.
     # Output shape = [-1, 3, 3, 20]
     with tf.name_scope('pool2'):
-        h_pool2 = max_pool_2x2(h_conv2)
+        S2 = max_pool_2x2(C2)
 
     # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
     # is down to 3x3x20 feature maps -- maps this to 100 features.
     with tf.name_scope('fc1'):
-        W_fc1 = weight_variable([3 * 3 * 20, 100])
-        b_fc1 = bias_variable([100])
+        W_F3 = weight_variable([3 * 3 * 20, 100])
+        b_F3 = bias_variable([100])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 3 * 3 * 20])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        S2_flat = tf.reshape(S2, [-1, 3 * 3 * 20])
+        h_fc1 = tf.nn.relu(tf.matmul(S2_flat, W_F3) + b_F3)
 
     # Map the 100 features to 10 classes, one for each digit
     with tf.name_scope('fc2'):
-        W_fc2 = weight_variable([100, 10])
-        b_fc2 = bias_variable([10])
+        W_F4 = weight_variable([100, 10])
+        b_F4 = bias_variable([10])
 
-        y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
+        F4 = tf.matmul(h_fc1, W_F4) + b_F4
     if save_image:
-        return h_conv1, h_pool1, h_conv2, h_pool2
-    return y_conv
+        return C1, S1, C2, S2
+    return F4
 
 
 def conv2d(x, W):
@@ -109,27 +109,41 @@ def main(_):
     x = tf.placeholder(tf.float32, [None, 784])
 
     # Define loss and optimizer
-    y_ = tf.placeholder(tf.float32, [None, 10])
+    y = tf.placeholder(tf.float32, [None, 10])
 
     # Build the graph for the deep net
-    y_conv = deepnn(x)
-    
+    F4 = deepnn(x)
+
     # for Images
     feature_maps = deepnn(x, True)
 
     with tf.name_scope('loss'):
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_,
-                                                                logits=y_conv)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y,
+                                                                logits=F4)
     cross_entropy = tf.reduce_mean(cross_entropy)
     vars = tf.trainable_variables()
     lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in vars if 'weight' in v.name]) * 0.0001
     cross_entropy += lossL2
+    global_step = tf.Variable(0, trainable=False)
     if config == 1:
+        decay_steps = int(batch_size*num_epochs)
+        decayed_learning_rate = tf.train.exponential_decay(learning_rate,
+                                                           global_step,
+                                                           decay_steps,
+                                                           decay_param,
+                                                           staircase=True)
         with tf.name_scope('Optimizer'):
-            train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+            train_step = tf.train.GradientDescentOptimizer(decayed_learning_rate).minimize(
+                cross_entropy)
     elif config == 2:
+        decay_steps = int(batch_size*num_epochs)
+        decayed_learning_rate = tf.train.exponential_decay(learning_rate,
+                                                           global_step,
+                                                           decay_steps,
+                                                           decay_param,
+                                                           staircase=True)
         with tf.name_scope('Optimizer'):
-            train_step = tf.train.MomentumOptimizer(learning_rate, momentum_param).minimize(
+            train_step = tf.train.MomentumOptimizer(decayed_learning_rate, momentum_param).minimize(
                 cross_entropy)
     elif config == 3:
         with tf.name_scope('Optimizer'):
@@ -137,7 +151,7 @@ def main(_):
                                                    epsilon=1e-6,).minimize(cross_entropy)
 
     with tf.name_scope('accuracy'):
-        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+        correct_prediction = tf.equal(tf.argmax(F4, 1), tf.argmax(y, 1))
         correct_prediction = tf.cast(correct_prediction, tf.float32)
     accuracy = tf.reduce_mean(correct_prediction)
 
@@ -149,9 +163,9 @@ def main(_):
         for epoch in range(num_epochs):
             for i in range(num_iters):
                 batch = mnist.train.next_batch(batch_size)
-                sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
-            l = sess.run(cross_entropy, feed_dict={x: trX, y_: trY})
-            acc = sess.run(accuracy, feed_dict={x: teX, y_: teY})
+                sess.run(train_step, feed_dict={x: batch[0], y: batch[1]})
+            l = sess.run(cross_entropy, feed_dict={x: trX, y: trY})
+            acc = sess.run(accuracy, feed_dict={x: teX, y: teY})
             train_errors.append(l)
             test_accuracy.append(acc)
             print('epoch %d: loss = %.2f, accuracy = %.2f' % (epoch, l, acc))
@@ -159,7 +173,7 @@ def main(_):
                   ylabel="Categorical Cross Entropy Loss")
         save_plot(test_accuracy, "cnn_classify_%d_test_acc" % (config), label="test_accuracy",
                   ylabel="Accuracy")
-        C1, S1, C2, S2 = sess.run(feature_maps, feed_dict={x: teX, y_: teY})
+        C1, S1, C2, S2 = sess.run(feature_maps, feed_dict={x: teX, y: teY})
         names = ["C1", "S1", "C2", "S2"]
         i = 0
         for arr, shape in zip([C1, S1, C2, S2], [[5, 6], [5, 6], [5, 8], [5, 8]]):
